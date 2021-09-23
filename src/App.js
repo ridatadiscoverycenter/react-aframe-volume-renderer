5ff8102b7f6211bdb0defbcf4445910dd891f50c
RobertGemmaJr <rgemma.jr@gmail.com>
2021-09-23 11:14:22 -0400

    refractor: delete layout component
    Simplification of component tree means an out <Layout> component is not needed

--- src/App.js
+++ src/App.js
@@ -7,13 +7,22 @@ import './components/Aframe/my-loader.js'
 import './components/Aframe/my-buttons-check.js'
 import './components/Aframe/render-2d-clipplane'
 import './components/Aframe/cursor-listener'
-import Layout from './components/React/Layout'
 
 import './styles/main.scss'
+
+import Header from './components/React/Header';
+import Visualizer from './components/React/Visualizer';
+import Footer from './components/React/Footer'
+import Information from "./components/React/Information";
 
 
 export default function App() {
   return (
-    <Layout />
+    <div id="visualizer">
+      <Header />
+      <Visualizer />
+      <Information />
+      <Footer />
+    </div>
   );
 }
