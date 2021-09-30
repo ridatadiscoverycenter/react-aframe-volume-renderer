533b040414b2b455ea61e6c6defd89c8d8b73ac4
RobertGemmaJr <rgemma.jr@gmail.com>
2021-09-30 17:38:24 -0400

    refractor: remove Layout component
    Outermost components are held directly in App.js

--- src/App.js
+++ src/App.js
@@ -7,10 +7,21 @@ import "./components/Aframe/my-loader.js";
 import "./components/Aframe/my-buttons-check.js";
 import "./components/Aframe/render-2d-clipplane";
 import "./components/Aframe/cursor-listener";
-import Layout from "./components/React/Layout";
+
+import Header from "./components/React/Header";
+import VolumeViewer from "./components/React/VolumeViewer";
+import Instructions from "./components/React/Instructions";
+import Footer from "./components/React/Footer";
 
 import "./styles/main.scss";
 
 export default function App() {
-  return <Layout />;
+  return (
+    <div id="visualizer">
+      <Header />
+      <VolumeViewer />
+      <Instructions />
+      <Footer />
+    </div>
+  )
 }
