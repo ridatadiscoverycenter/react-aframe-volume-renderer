11389c2a25b0280474a3a79c44fda2ca6eeeebbf
RobertGemmaJr <rgemma.jr@gmail.com>
2021-09-30 17:30:09 -0400

    refractor: move instructions to serperate component

---
+++ src/components/React/Instructions.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { Button, Container, Row, Col } from "react-bootstrap";
+
+import Howto from "./Howto";
+
+export default function Instructions(props) {
+  const [showHowto, setShowHowto] = useState(false);
+
+  return (
+      <Container fluid id="information" className="bg-secondary text-light">
+      <Row className="justify-content-md-center py-5">
+        <Col xs={8}>
+          <div className="d-flex flex-column">
+            <h1 className="my-4">Narragansett Bay Volume Renderer</h1>
+            <p className="my-4">
+              The main goal of the project is to provide an easily accessible
+              and interactive environment to explore and showcase volumetric
+              Naragansett Bay data with the added benefits of Virtual Reality
+              If VR-capable hardware is available.
+              <br />
+            </p>
+
+            <h2 className="mt-4 text-center">
+              Learn How To Use the Volume Renderer
+            </h2>
+            <Button
+              variant="primary"
+              onClick={() => setShowHowto(true)}
+              className="mb-4"
+            >
+              Launch Instructions
+            </Button>
+            <Howto show={showHowto} close={() => setShowHowto(false)} />
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
--- src/components/React/Layout.js
+++ src/components/React/Layout.js
@@ -1,15 +1,15 @@
-import React, { useState } from "react";
-import { Navbar, Nav, Button, Container, Row, Col } from "react-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
 
 import VolumeRenderer from "./VolumeRenderer";
 import ControlsPanel from "./ControlsPanel";
-import Howto from "./Howto";
 import Footer from "./Footer";
+import Instructions from "./Instructions";
 
 import { VolumeProvider } from "../../context/volume-context";
 
+
 export default function Layout(props) {
-  const [showHowto, setShowHowto] = useState(false);
+  
 
   return (
     <div id="visualizer">
@@ -37,37 +37,7 @@ export default function Layout(props) {
         </VolumeProvider>
       </Container>
 
-      <Container fluid id="information" className="bg-secondary text-light">
-        <Row className="justify-content-md-center py-5">
-          <Col xs={8}>
-            <div className="d-flex flex-column">
-              <h1 className="my-4">Narragansett Bay Volume Renderer</h1>
-
-              <p className="my-4">
-                The main goal of the project is to provide an easily accessible
-                and interactive environment to explore and showcase volumetric
-                Naragansett Bay data with the added benefits of Virtual Reality
-                If VR-capable hardware is available.
-                <br />
-              </p>
-
-              <h2 className="mt-4 text-center">
-                Learn How To Use the Volume Renderer
-              </h2>
-              <Button
-                variant="primary"
-                onClick={() => setShowHowto(true)}
-                className="mb-4"
-              >
-                Launch Instructions
-              </Button>
-
-              <Howto show={showHowto} close={() => setShowHowto(false)} />
-            </div>
-          </Col>
-        </Row>
-      </Container>
-
+      <Instructions />
       <Footer />
     </div>
   );
