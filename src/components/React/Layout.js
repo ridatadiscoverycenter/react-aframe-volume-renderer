ac94c341d2ac2b5d3c187fdb1046fc9191481906
RobertGemmaJr <rgemma.jr@gmail.com>
2021-09-30 17:35:43 -0400

    refractor: move Header into a seperate component

--- src/components/React/Layout.js
+++ src/components/React/Layout.js
@@ -1,28 +1,13 @@
-import { Navbar, Nav} from "react-bootstrap";
-
+import Header from "./Header";
 import VolumeViewer from "./VolumeViewer";
 import Instructions from "./Instructions";
 import Footer from "./Footer";
 
+
 export default function Layout(props) {
   return (
     <div id="visualizer">
-      <Navbar sticky="top" bg="light" expand="lg">
-        <Navbar.Brand href="https://ridatadiscovery.org">
-          <img
-            src="/assets/images/ricaim-logo.svg"
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-            alt="Rhode Island Data Discovery Center"
-          />
-        </Navbar.Brand>
-        <Nav>
-          <Nav.Link href="#visualizer">Visualizer</Nav.Link>
-          <Nav.Link href="#information">Info</Nav.Link>
-        </Nav>
-      </Navbar>
-
+      <Header />
       <VolumeViewer />
       <Instructions />
       <Footer />
