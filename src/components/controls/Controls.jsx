6115a2a7761172a0414815fa3289a489aa068256
RobertGemmaJr <rgemma.jr@gmail.com>
2021-10-21 10:40:35 -0400

    refractor: move clip controls into thir own component
    This matches the structure of the other 2 controls

--- src/components/controls/Controls.jsx
+++ src/components/controls/Controls.jsx
@@ -1,26 +1,10 @@
-import { Container, Form, Row } from "react-bootstrap";
-import Slider from "rc-slider";
-import "rc-slider/assets/index.css";
+import { Container, Row } from "react-bootstrap";
 
-import { useControlsContext } from "../../context/controls-context";
 import OpacityControls from "./OpacityControl";
 import ColorMapControls from "./ColorMapControls";
+import ClipControls from "./ClipControls";
 
 export default function Controls(props) {
-  const {
-    state: { sliderRange },
-    dispatch,
-  } = useControlsContext();
-
-  function handleClick(axis, val) {
-    dispatch({
-      type: `CHANGE_SLIDER`,
-      payload: {
-        [axis.concat("SliderBounds")]: val,
-      },
-    });
-  }
-
   return (
     <Container id="controls" className="p-3">
       <Row noGutters className="mb-3">
@@ -30,24 +14,7 @@ export default function Controls(props) {
         <OpacityControls />
       </Row>
       <Row noGutters className="mt-5">
-        <h4>Clip</h4>
-        <Form className="fullWidth">
-          {["x", "y", "z"].map((axis) => {
-            return (
-              <Form.Group key={axis}>
-                <Form.Label>{axis.toUpperCase()}</Form.Label>
-                <Slider.Range
-                  allowCross={false}
-                  step={0.0009}
-                  defaultValue={[sliderRange.min, sliderRange.max]}
-                  min={sliderRange.min}
-                  max={sliderRange.max}
-                  onChange={(val) => handleClick(axis, val)}
-                />
-              </Form.Group>
-            );
-          })}
-        </Form>
+        <ClipControls />
       </Row>
     </Container>
   );
