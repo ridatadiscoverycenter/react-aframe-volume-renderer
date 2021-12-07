8778e654e72aad573c5512767fc69da9d085b249
RobertGemmaJr <rgemma.jr@gmail.com>
2021-12-07 11:03:33 -0500

    chore: decunstruct config to change export schema

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,10 @@
-import config from "../assets/config.json";
-import MODEL_DATA from "../assets/volume-min-max.json";
+import { season, tide, measurement, model } from "../assets/config.json";
+
+const BUTTONS = {
+  season: season,
+  tide: tide,
+  measurement: measurement,
+};
 
 // Import all colorMaps from '../assets/colormaps/' as {fileName: path}
 const colorMaps = require.context(
@@ -15,12 +20,6 @@ const ALL_COLOR_MAPS = colorMaps.keys().reduce(
   {}
 );
 
-const BUTTONS = {
-  season: config.season,
-  tide: config.tide,
-  measurement: config.measurement,
-};
-
-const MODEL_CONSTANTS = config.model;
-
-export { ALL_COLOR_MAPS, BUTTONS, MODEL_CONSTANTS, MODEL_DATA };
+export { ALL_COLOR_MAPS, BUTTONS };
+export { model as MODEL_CONSTANTS };
+export * as MODEL_DATA from "../assets/volume-min-max.json";
