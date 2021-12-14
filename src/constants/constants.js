8128c17b6940d625ba0c560a6a69f66a15b78148
RobertGemmaJr <rgemma.jr@gmail.com>
2021-12-14 13:50:57 -0500

    refractor: change import style from json

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,9 +1,9 @@
-import { season, tide, measurement, model } from "../assets/config.json";
+import config from "../assets/config.json";
 
 const BUTTONS = {
-  measurement: measurement,
-  season: season,
-  tide: tide,
+  measurement: config.measurement,
+  season: config.season,
+  tide: config.tide,
 };
 
 // Import all colorMaps from '../assets/colormaps/' as {fileName: path}
@@ -20,6 +20,7 @@ const ALL_COLOR_MAPS = colorMaps.keys().reduce(
   {}
 );
 
-export { ALL_COLOR_MAPS, BUTTONS };
-export { model as MODEL_CONSTANTS };
+const MODEL_CONSTANTS = config.model
+
+export { ALL_COLOR_MAPS, BUTTONS, MODEL_CONSTANTS };
 export * as MODEL_DATA from "../assets/volume-min-max.json";
