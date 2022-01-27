0c033d9c4071b87140c114039296684b4aeca1b2
RobertGemmaJr <rgemma.jr@gmail.com>
2022-01-27 13:41:41 -0500

    refractor: update how ALL_COLOR_MAPS are created
    Needed to work with the updated react-volume-viewer package

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -7,18 +7,18 @@ const BUTTONS = {
 };
 
 // Import all colorMaps from '../assets/colormaps/' as {fileName: path}
+// TODO: Should be {name: [name], path: [path]}
 const colorMaps = require.context(
   "../assets/colormaps/",
   false,
   /\.(png|jpe?g|svg)$/
 );
-const ALL_COLOR_MAPS = colorMaps.keys().reduce(
-  (obj, image) => ({
-    ...obj,
-    [image.replace("./", "").split(".")[0]]: colorMaps(image).default,
-  }),
-  {}
-);
+const ALL_COLOR_MAPS = colorMaps.keys().map((image) => {
+  return {
+    name: image.replace("./", "").split(".")[0],
+    path: colorMaps(image).default,
+  };
+});
 
 export { ALL_COLOR_MAPS, BUTTONS };
 export { model as MODEL_CONSTANTS };
